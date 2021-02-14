a63d350eec824aa7101f64797c3a8282d1b43a35
Changbeom Ahn <kewlbear@gmail.com>
2021-02-14 22:24:53 +0900

    WIP

--- LAPACKE/iOS.sh
+++ LAPACKE/iOS.sh
@@ -1,1 +1,2 @@
-make CC="xcrun -sdk iphoneos clang -arch arm64" LAPACKELIB=liblapacke-iOS.a
+#make CC="xcrun -sdk iphoneos clang -arch arm64" LAPACKELIB=liblapacke-iOS.a
+make -j4 CC="xcrun -sdk iphonesimulator clang -arch arm64" LAPACKELIB=liblapacke-iOS-simulator.a
