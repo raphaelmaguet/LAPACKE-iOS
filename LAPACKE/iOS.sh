6c828afb25e94f291ce26fbd14597f184b15f9c2
Changbeom Ahn <kewlbear@gmail.com>
2021-02-23 10:11:57 +0900

    Swift package

--- LAPACKE/iOS.sh
+++ LAPACKE/iOS.sh
@@ -1,8 +1,10 @@
-make cleanobj all CC="xcrun --sdk iphoneos clang -arch arm64" LAPACKELIB=liblapacke-iOS.a CFLAGS="-mios-version-min=9.0"
+DEPLOYMENT_TARGET=9.0
 
-make cleanobj all CC="xcrun --sdk iphonesimulator clang -arch x86_64" LAPACKELIB=liblapacke-iOS-simulator-x86_64.a CFLAGS="-miphonesimulator-version-min=9.0"
+make clean all CC="xcrun --sdk iphoneos clang -arch arm64" LAPACKELIB=liblapacke-iOS.a CFLAGS="-mios-version-min=$DEPLOYMENT_TARGET"
 
-make cleanobj all CC="xcrun --sdk iphonesimulator clang -arch arm64" LAPACKELIB=liblapacke-iOS-simulator-arm64.a CFLAGS="-miphonesimulator-version-min=9.0"
+make clean all CC="xcrun --sdk iphonesimulator clang -arch x86_64" LAPACKELIB=liblapacke-iOS-simulator-x86_64.a CFLAGS="-miphonesimulator-version-min=$DEPLOYMENT_TARGET"
+
+make clean all CC="xcrun --sdk iphonesimulator clang -arch arm64" LAPACKELIB=liblapacke-iOS-simulator-arm64.a CFLAGS="-miphonesimulator-version-min=$DEPLOYMENT_TARGET"
 
 lipo -create -arch x86_64 ../liblapacke-iOS-simulator-x86_64.a -arch arm64 ../liblapacke-iOS-simulator-arm64.a -output ../liblapacke-iOS-simulator.a
 
