41eb10f9cc4ff1d0c31ec3739545232435f384bf
Changbeom Ahn <kewlbear@gmail.com>
2021-02-23 10:13:44 +0900

    Swift package

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "LAPACKE-iOS",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(
+            name: "LAPACKE-iOS",
+            targets: ["LAPACKE", "Accelerate"]),
+    ],
+    targets: [
+        .binaryTarget(name: "LAPACKE", path: "LAPACKE.xcframework"),
+        .target(
+            name: "Accelerate",
+            linkerSettings: [.linkedFramework("Accelerate")]),
+    ]
+)
