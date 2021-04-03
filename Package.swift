42a45bda301e1a20f72e2eb0f1c18a8050d4067d
Changbeom Ahn <kewlbear@gmail.com>
2021-04-03 15:59:59 +0900

    fix Accelerate name clash

--- Package.swift
+++ Package.swift
@@ -10,12 +10,12 @@ let package = Package(
     products: [
         .library(
             name: "LAPACKE-iOS",
-            targets: ["LAPACKE", "Accelerate"]),
+            targets: ["LAPACKE", "_LapackeLink"]),
     ],
     targets: [
         .binaryTarget(name: "LAPACKE", url: "https://github.com/kewlbear/LAPACKE-iOS/releases/download/0.0.1-20210331/LAPACKE.xcframework.zip", checksum: "cd2f078c8213a5925a1c74b2f63212fe7a900bc87b08a4e4ccd1069520ac6959"),
         .target(
-            name: "Accelerate",
+            name: "_LapackeLink",
             linkerSettings: [.linkedFramework("Accelerate")]),
     ]
 )
